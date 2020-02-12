02c79b29098ca68b39d4e49654fe5b694f649d10
Alasdair Tran <alasdair.tran@gmail.com>
2020-02-12 17:51:11 +1100

    Rename Genre to GenreOverview

--- frontend/src/scenes/Pages/Overview/index.js
+++ frontend/src/scenes/Pages/Overview/index.js
@@ -4,7 +4,7 @@ import { Route } from 'react-router-dom';
 // DASHBOARDS
 
 import BasicOverview from './Basic/';
-import GenreOverview from './Genre';
+import GenreOverview from './GenreOverview';
 import ArtistOverview from './ArtistOverview';
 import SongOverview from './SongOverview';
 import SongEgo from './SongEgo';
